15d60a216c8adfc50878c22d1891a23564c46329
Yuxuan (Jacky) Zou <zouyuxuanjk0410@gmail.com>
2026-02-11 01:15:33 -0800

    Add files via upload

---
+++ __a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\zouyu\Desktop\Elec291\Elec291 project 1\"
+"C:\Users\zouyu\Desktop\Elec291\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\zouyu\Desktop\Elec291\Elec291 project 1\fsm777 - 副本.asm"
+echo Crosside_Action Set_Hex_File C:\Users\zouyu\Desktop\Elec291\Elec291 project 1\fsm777 - 副本.HEX
